f68ef95496a751ec805745688182b4d8b53ee2d8
AtharvKadam21 <atharvkadam82@gmail.com>
2023-10-30 17:56:41 +0530

    react router

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import About from './About/About.jsx'
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import User from './User/User.jsx'
+import Home from './Home/Home.jsx'
+import Contact from './Contact/Contact.jsx'
+
+export {About, Header, Footer, User, Home, Contact}
